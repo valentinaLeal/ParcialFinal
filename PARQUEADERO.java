3435822463685fc35ef1fe69567c7777651f91f3
valentinaleal <36080448+valentinaLeal@users.noreply.github.com>
2018-06-01 12:03:35 -0500

    Add files via upload

---
+++ PARQUEADERO.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial;
+
+/**
+ *
+ * @author Estudiantes
+ */
+public class PARQUEADERO {
+    
+    VEHICULO misVehiculos []= new VEHICULO [100];
+    
+}
---
+++ VEHICULO.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial;
+
+
+public class VEHICULO {
+     
+    
+    int retirarVehiculo (){
+        
+   int tiempoEntrada;
+    int tiempoSalida;
+ int valorPagar;
+     
+    tiempoSalida= 0;
+    tiempoEntrada= 0;
+    
+   (horaSalida - horaEntrada) * 100 = valorPagar;
+   
+    }return valorPagar;
+    
+}
